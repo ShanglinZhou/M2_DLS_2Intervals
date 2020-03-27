92c962b20d1f6ad73e9eb38223ac8837e9ab0b2b
ShanglinZhou <zhoushanglin@gmail.com>
2020-03-26 17:05:31 -0700

    Add files via upload

---
+++ SeqIndexDB.m
@@ -0,0 +1,51 @@
+function [SqI, PE, TS] = SeqIndexDB(Data,NumEntropyBins)
+%The Sequentiality Index (SqI) is is the product of two entropy measures:
+% entropy of the distribution of peak activity time (Peak Entropy) and entropy of the
+% spatsity of neural activity (Temporal Sparsity). The peak entropy will be maximal if the
+% there is a uniform distribution of neurons whose peaks tile the entire
+% delay period. One minus the sparsiity entropy will me maxminal when the activity at
+% each moment comes from a single neuron. Thus SqI is maximal when the
+% neural sequence is a perfect diagonal line across the entire window of
+% interest with no overalap between neurons (a perfect "neural chain")
+% Inputs:
+% Data: a 3D matrix of neural activity in which index of trials, neurons,
+% and time are in 1, 2, 3 dimension repectively
+% NumEntropyBins: the number of entrpopy bins used to estimate the
+% distribution of peak times and the Peak Entropy.
+
+% Outputs:
+% SqI: Sequentiality Index
+% PE: Peak Entropy
+% TS: mean Temporal Sparsity
+% Dean Buonomano 9/14/2019
+
+[numTrials,numUnits, max_t]=size(Data);
+binsize = (max_t/(NumEntropyBins-1));
+
+
+for i = 1:numTrials
+    data_temp = squeeze(Data(i,:,:));% data for single trial
+    
+    % PEAK ENTROPY
+    [peakval, peaktimes]=max(data_temp,[],2);
+    [N, X]=hist(peaktimes,0:binsize:max_t);
+    probpeak = N./numUnits;
+    probpeak = probpeak(probpeak>0);
+    pe = sum(-probpeak.*log2(probpeak));
+    PE(i) = pe/log2(NumEntropyBins);  %Normalized Peak Entropy
+    
+    % MEAN TEMPORAL SPARSITY
+    dn = data_temp./sum(data_temp); % normalized activity at each time
+    ts = nansum(-dn.*log2(dn));
+    ts = mean(ts);
+    TS(i) = 1 - ts./log2(numUnits); %normalized temporal sparsity
+    
+    SqI(i) = sqrt(PE(i)*TS(i));
+end
+SqI = mean(SqI);
+PE = mean(PE);
+TS = mean(TS);
+
+
+
+
---
+++ abscIndex.m
@@ -0,0 +1,31 @@
+function ASI = abscIndex(psth1,psth2)
+% The absolute and scaling index (ASI) is to measure the absolute (or scaling) level of
+% the responses of one neuron to two intervals with different length.
+% psth1: population dynamics for short interval;1-d neuron; 2-d time
+% psth2: population dynamics for long interval;1-d neuron; 2-d time
+% By Shanglin Zhou
+maxTime1 = size(psth1,2);
+maxTime2 = size(psth2,2);
+numCell = size(psth1,1);
+
+abscDist = zeros(numCell,maxTime1-1);%Euclidean distance between psth1 and time-warped psth2
+AbsR =  zeros(numCell,maxTime1-1);% absoulte ratio
+for i = 1:maxTime1-1
+    psth2Temp = [psth2(:,1:i) psth2(:,int16(linspace(i+1,maxTime2,maxTime1-i)))];% time-warped psth2 at break point i
+    temp1 = (psth1-psth2Temp).^2;
+    abscDist(:,i) = sqrt(sum(temp1,2));
+    
+    W_abs = mean(abs((psth1(:,1:i)-psth1(:,1)).*(psth2Temp(:,1:i)-psth2Temp(:,1))),2);
+    W_scale = mean(abs((psth1(:,i+1:end)-psth1(:,i+1)).*(psth2Temp(:,i+1:end)-psth2Temp(:,i+1))),2);
+    AbsR(:,i) = W_abs./(W_abs + W_scale);
+end
+
+AbsR(isnan(AbsR)) = 0.5;
+
+[~,minInd] = min(abscDist,[],2);% minimun distance between psth1 and time-warped psth2 at minInd
+temporal_Index = minInd./maxTime1;
+
+AbsR_Index = AbsR(sub2ind(size(AbsR),1:numCell,minInd'));
+
+ASI = (temporal_Index + AbsR_Index')/2;
+end
